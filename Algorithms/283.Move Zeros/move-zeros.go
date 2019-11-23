927f071623d6102636739224120a0139f7d38b8c
HaoQian95 <1518375684@qq.com>
2019-11-23 22:46:20 +0800

    283 finish

---
+++ Algorithms/283.Move Zeros/move-zeros.go
@@ -0,0 +1,15 @@
+package problem283
+
+func moveZeroes(nums []int)  {
+	position := 0
+	for _ , num := range(nums) {
+		if num != 0 {
+			nums[position] = num
+			position++
+		}
+	}
+	for position < len(nums) {
+		nums[position] = 0
+		position++
+	}
+}
